f8e1e5faa218d450d78d1e02bd6a251dcb51c13c
aegishield <prasetyo.raharja12@gmail.com>
2018-03-05 15:23:29 +0800

    initialize main frame TODO : add another operation

--- Modul1.1.c
+++ Modul1.1.c
@@ -1,36 +1,54 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-void menu();
-void calc(int choice);
+void menu(bool isFirst,double lastSum);
+void calc(bool isFirst,int choice,double lastSum);
 double pertambahan(int num1,int num2);
 
 int main(){
+    bool isFirst = true;
     printf("Selamat datang di program kalkulator!");
-    menu();
+    menu(isFirst,0);
     return 0;
 }
 
-void menu(){
+void menu(bool isFirst,double lastSum){
     int choice;
-    printf("\n\nPilih operasi:\n1.Pertambahan\n2.Pengurangan\n3.Perkalian\n4.Pembagian\n5.Modulus\n\nPilihan : ");
-    scanf("%d",choice);
-    calc(choice);
+    printf("\n\nPilih operasi:\n1.Pertambahan\n2.Pengurangan\n3.Perkalian\n4.Pembagian\n5.Modulus\n6.Exit\n\nPilihan : ");
+    scanf("%d",&choice);
+    calc(isFirst,choice,lastSum);
 }
 
-void calc(int choice){
+void calc(bool isFirst,int choice,double lastSum){
     double num1;
     double num2;
     double sum;
-    printf("Masukkan angka pertama : ");
-    scanf("%lf",num1);
-    printf("Masukkan angka kedua : ");
-    scanf("%lf",num2);
+    if (choice == 6){
+        return;
+    }
+    else if (isFirst){
+        printf("Masukkan angka pertama : ");
+        scanf("%lf",&num1);
+        printf("Masukkan angka kedua : ");
+        scanf("%lf",&num2);
+    }
+    else {
+        num1 = lastSum;
+        printf("Masukkan angka kedua : ");
+        scanf("%lf",&num2);
+    }
+    isFirst = false;
     switch(choice){
         case 1 :
             sum = pertambahan(num1,num2);
             break;
+        case 2 :
+            sum = pengurangan(num1,num2);
     }
-    printf("%d",sum);
+    system("cls");
+    printf("Hasil operasi :%lf",sum);
+    menu(isFirst,sum);
 }
 
 double pertambahan(int num1,int num2){
