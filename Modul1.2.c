3ca25748be5fe64e711334d3e4b652370f7121d7
aegishield <prasetyo.raharja12@gmail.com>
2018-03-05 22:29:13 +0800

    implement mainframe TODO implement function

--- Modul1.2.c
+++ Modul1.2.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
+void menu;
+
+typedef struct bangunDatar
+{
+    double luas;
+    double keliling; 
+} bangunDatar;
+
+bangunDatar persegi();
+bangunDatar segitiga();
+bangunDatar lingkaran();
+
 int main(){
-    printf("%lf",4.0/3.0*3.16*14*14*14);
+
     return 0;
 }
+
+void menu(){
+    int choice;
+    bangunDatar pilihan;
+    printf("Pilih bangun ruang:\n\n1.Balok\n2.Bola\n3.Limas segitiga sama sisi\n4.Prisma segitiga\n5.Tabung\n\nPilihan : ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1:
+            pilihan = persegi();
+            break;
+        case 2:
+            pilihan = segitiga();
+            break;
+        case 3:
+            pilihan = lingkaran();
+            break;
+        case 4:
+            pilihan = trapesium();
+            break;
+        case 5:
+            pilihan = jajarGenjang();
+            break;
+    }
+}
+
