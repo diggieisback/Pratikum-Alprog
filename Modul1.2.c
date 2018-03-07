b2510c126e8418814690860785560eb10fabc8b9
aegishield <prasetyo.raharja12@gmail.com>
2018-03-07 12:37:51 +0800

    finished program, waiting for update

--- Modul1.2.c
+++ Modul1.2.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
-
-void menu;
+#include <string.h>
+#include <math.h>
 
 typedef struct bangunDatar
 {
     double luas;
     double keliling; 
+    char nama[20];
 } bangunDatar;
 
+double PHI=3.14;
+
+void menu();
 bangunDatar persegi();
 bangunDatar segitiga();
 bangunDatar lingkaran();
+bangunDatar jajarGenjang();
+bangunDatar trapesium();
+
 
 int main(){
-
+    menu();
     return 0;
 }
 
 void menu(){
     int choice;
     bangunDatar pilihan;
-    printf("Pilih bangun ruang:\n\n1.Balok\n2.Bola\n3.Limas segitiga sama sisi\n4.Prisma segitiga\n5.Tabung\n\nPilihan : ");
+    printf("Pilih bangun datar:\n\n1.Persegi\n2.Segitiga\n3.Lingkaran\n4.Trapesium\n5.Jajar Genjang\n\nPilihan : ");
     scanf("%d",&choice);
     switch (choice)
     {
@@ -40,5 +47,73 @@ void menu(){
             pilihan = jajarGenjang();
             break;
     }
+    printf("Bangun datar %s : \nLuas : %.2lf satuan\nKeliling : %.2lf satuan",pilihan.nama,pilihan.luas,pilihan.keliling);
 }
 
+bangunDatar persegi(){
+    bangunDatar persegi;
+    double panjang,tinggi;
+    printf("Masukkan panjang : ");
+    scanf("%lf",&panjang);
+    printf("Masukkan tinggi : ");
+    scanf("%lf",&tinggi);
+    persegi.luas = panjang * tinggi;
+    persegi.keliling = 2 * (panjang + tinggi);
+    strcpy(persegi.nama,"Persegi");
+    return persegi;
+}
+
+bangunDatar lingkaran(){
+    bangunDatar lingkaran;
+    double r;
+    printf("Masukkan panjang jari-jari : ");
+    scanf("%lf",&r);
+    lingkaran.luas = PHI * r * r;
+    lingkaran.keliling = 2 * PHI * r;
+    strcpy(lingkaran.nama,"Lingkaran");
+    return lingkaran;
+}
+
+bangunDatar segitiga(){
+    bangunDatar segitiga;
+    double s1,s2,s3,s;
+    printf("Masukkan semua sisi segitiga : ");
+    scanf("%lf %lf %lf",&s1,&s2,&s3);
+    segitiga.keliling = s1 + s2 + s3;
+    s = 1.0/2.0 * segitiga.keliling;
+    segitiga.luas = sqrt(s*(s-s1)*(s-s2)*(s-s3)); 
+    strcpy(segitiga.nama,"Segitiga");
+    return segitiga;
+}
+
+bangunDatar jajarGenjang(){
+    bangunDatar jajarGenjang;
+    double s1,s2,t;
+    printf("Masukkan sisi alas jajar genjang : ");
+    scanf("%lf",&s1);
+    printf("Masukkan sisi miring jajar genjang : ");
+    scanf("%lf",&s2);
+    printf("Masukkan tinggi jajar genjang : ");
+    scanf("%lf", &t);
+    jajarGenjang.luas = s1 * t;
+    jajarGenjang.keliling = 2 * s1 + 2 * s2 ;
+    strcpy(jajarGenjang.nama,"Jajar Genjang");
+    return jajarGenjang;
+}
+
+bangunDatar trapesium(){
+    bangunDatar trapesium;
+    double alas,sisiSejajar,sisiLain1,sisiLain2,tinggi;
+    printf("Masukkan alas : ");
+    scanf("%lf",&alas);
+    printf("Masukkan sisi sejajar dengan alas : ");
+    scanf("%lf",&sisiSejajar);
+    printf("Masukkan 2 buah sisi lainnya : ");
+    scanf("%lf %lf",&sisiLain1,&sisiLain2);
+    printf("Masukkan tinggi :");
+    scanf("%lf",&tinggi);
+    trapesium.keliling = alas + sisiSejajar + sisiLain1 + sisiLain2;
+    trapesium.luas = 1.0/2.0 * (alas + sisiSejajar) * tinggi;
+    strcpy(trapesium.nama,"Trapesium");
+    return trapesium;
+}
