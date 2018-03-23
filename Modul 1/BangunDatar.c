7f1ed8356191c2679125eda5de74e8c9114f1e4f
aegishield <prasetyo.raharja12@gmail.com>
2018-03-23 13:57:21 +0800

    fix

--- Modul 1/BangunDatar.c
+++ Modul 1/BangunDatar.c
@@ -1,198 +1,395 @@
 #include <stdio.h>
+
 #include <string.h>
+
 #include <math.h>
+
 #include <stdbool.h>
+
 #include <stdlib.h>
 
+
+
 typedef struct bangunDatar
+
 {
+
     double luas;
+
     double keliling; 
+
     char nama[20];
+
 } bangunDatar;
+
+
 
 double PHI=3.14;
 
+
+
 void menu();
+
 bangunDatar persegi();
+
 bangunDatar segitiga();
+
 bangunDatar lingkaran();
+
 bangunDatar jajarGenjang();
+
 bangunDatar trapesium();
+
 double input();
+
 void clear_buffer();
+
 void reset();
 
+
+
 int main(){
+
     menu();
+
     return 0;
+
 }
+
+
 
 void menu(){
+
     float choice;
+
     int check;
 
+
+
     bangunDatar pilihan;
+
     printf("Pilih bangun datar:\n\n1.Persegi\n2.Segitiga\n3.Lingkaran\n4.Trapesium\n5.Jajar Genjang\n6.Exit Program\n\nPilihan : ");
+
     choice = input();
+
     check = ceil(choice);
+
     if (check == choice){
+
         if (choice>5 || choice < 1){
+
                 if (choice == 6){
+
                         return;
+
                 } else {
+
                     system("cls");
+
                     printf("Input invalid !\n");
+
                     menu();
+
                 }
+
         } else {
+
         //Validasi dikuatkan(input float gagal,isi huruf di belakang error)
+
             switch ((int)choice)
+
             {
+
                 case 1:
+
                     pilihan = persegi();
+
                     break;
+
                 case 2:
+
                     pilihan = segitiga();
+
                     break;
+
                 case 3:
+
                     pilihan = lingkaran();
+
                     break;
+
                 case 4:
+
                     pilihan = trapesium();
+
                     break;
+
                 case 5:
+
                     pilihan = jajarGenjang();
+
                     break;
+
             }
+
             printf("\n\nBangun datar %s : \nLuas : %.2lf satuan\nKeliling : %.2lf satuan\n\n",pilihan.nama,pilihan.luas,pilihan.keliling);
+
             reset();
+
         }
+
     }else {
+
         system("cls");
+
         printf("Input invalid!\n");
+
         menu();
+
     }
+
 }
+
+
 
 bangunDatar persegi(){
+
     bangunDatar persegi;
+
     double panjang,tinggi;
+
     printf("Masukkan panjang : ");
+
     panjang = input();
+
     printf("Masukkan tinggi : ");
+
     tinggi = input();
+
     persegi.luas = panjang * tinggi;
+
     persegi.keliling = 2 * (panjang + tinggi);
+
     strcpy(persegi.nama,"Persegi");
+
     return persegi;
+
 }
+
+
 
 bangunDatar lingkaran(){
+
     bangunDatar lingkaran;
+
     double r;
+
     printf("Masukkan panjang jari-jari : ");
+
     r = input();
+
     lingkaran.luas = PHI * r * r;
+
     lingkaran.keliling = 2 * PHI * r;
+
     strcpy(lingkaran.nama,"Lingkaran");
+
     return lingkaran;
+
 }
+
+
 
 bangunDatar segitiga(){
+
     bangunDatar bangunSegitiga;
+
     double s1,s2,s3,s;
+
     printf("Masukkan sisi 1 : ");
+
     s1 = input();
+
     printf("Masukkan sisi 2 : ");
+
     s2 = input();
+
     printf("Masukkan sisi 3 : ");
+
     s3 = input();
+
     if(s1+ s2 == s3 || s1+s3 == s2 || s2 + s3 ==s1){
+
         system("cls");
+
         printf("Input invalid\n");
+
         bangunSegitiga = segitiga();
+
     }else{
+
         bangunSegitiga.keliling = s1 + s2 + s3;
+
         s = 0.5 * bangunSegitiga.keliling;
+
         bangunSegitiga.luas = sqrt(s*(s-s1)*(s-s2)*(s-s3)); 
+
         strcpy(bangunSegitiga.nama,"Segitiga");
+
     }
+
     return bangunSegitiga;
+
 }
+
+
 
 bangunDatar jajarGenjang(){
+
     bangunDatar jajarGenjang;
+
     double s1,s2,t;
+
     printf("Masukkan sisi alas jajar genjang : ");
+
     s1 = input();
+
     printf("Masukkan sisi miring jajar genjang : ");
+
     s2 = input();
+
     printf("Masukkan tinggi jajar genjang : ");
+
     t = input();
+
     jajarGenjang.luas = s1 * t;
+
     jajarGenjang.keliling = 2 * s1 + 2 * s2 ;
+
     strcpy(jajarGenjang.nama,"Jajar Genjang");
+
     return jajarGenjang;
+
 }
+
+
 
 bangunDatar trapesium(){
+
     bangunDatar trapesium;
+
     double alas,sisiSejajar,sisiLain1,sisiLain2,tinggi;
+
     printf("Masukkan alas : ");
+
     alas = input();
+
     printf("Masukkan sisi sejajar dengan alas : ");
+
     sisiSejajar = input();
+
     printf("Masukkan 2 buah sisi lainnya : ");
+
     sisiLain1 = input();
+
     sisiLain2 = input();
+
     printf("Masukkan tinggi :");
+
     tinggi = input();
+
     trapesium.keliling = alas + sisiSejajar + sisiLain1 + sisiLain2;
+
     trapesium.luas = 1.0/2.0 * (alas + sisiSejajar) * tinggi;
+
     strcpy(trapesium.nama,"Trapesium");
+
     return trapesium;
+
 }
 
+
+
 double input(){
+
     double num;
+
     char chara;
-    if (scanf("%lf%c",&num,&chara)!= 2 || chara != '\n' || chara == ','){
-        clear_buffer();
-        printf("Input invalid! Masukkan angka : ");
+
+    if (scanf("%lf%c",&num,&chara)!= 2 || num<=0 || chara != '\n' || chara == ','){
+
+        fflush(stdin);
+
+        printf("Input invalid! Masukkan angka yang benar : ");
+
         return input();
+
     } else {
+
         return num;
+
     }
+
 }
+
+
+
 
 
 void clear_buffer(){
+
     char c;
+
     while ((c = getchar()) != '\n' && c != EOF) ; 
+
     
+
 }
+
+
 
 void reset(){
 
+
+
     int valid_reset;
+
     char parameter[1];
 
+
+
     printf("Kembali menghitung?[Y/N] ");
+
     gets(parameter);
+
     
+
     if (!(strcmp(parameter,"Y")) || !(strcmp(parameter,"y"))){
+
         
+
         system("cls");
+
         menu();
 
+
+
     } else if (!(strcmp(parameter,"N")) || !(strcmp(parameter,"n"))){
+
         
+
         return;
 
+
+
     } else {
+
         system("cls");
+
         printf("Invalid Input !\n\a");
+
         reset();
+
     }
+
 }
--- Modul 1/Kalkulator.c
+++ Modul 1/Kalkulator.c
@@ -1,148 +1,297 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <math.h>
+
 #include <ctype.h>
 
 
+
+
+
 void menu();
+
 double pertambahan(double num1,double num2);
+
 double pengurangan(double num1,double num2);
+
 double perkalian(double num1,double num2);
+
 double pembagian(double num1,double num2);
+
 int modulus(double num1,double num2);
+
 void clear_buffer();
+
 void calc(int choice);
+
 double input();
+
 void reset();
 
 
+
+
+
 int main(){
+
     printf("Selamat datang di program kalkulator!");
+
     menu();
+
     return 0;
+
 }
+
+
 
 void menu(){
+
     double choice;
+
     int check;
+
     char buff;
+
     printf("\n\nPilih operasi:\n1.Pertambahan\n2.Pengurangan\n3.Perkalian\n4.Pembagian\n5.Modulus\n6.Exit\n\nPilihan : ");
+
     choice = input();
+
     check = ceil(choice);
+
     if (check == choice){
+
         if (choice>5 || choice < 1){
+
             if (choice == 6){
+
                     return;
+
             } else {
+
                 printf("Input invalid !");
+
                 menu();
+
             }
+
         } else {
+
             calc(choice);
+
             reset();
+
         }
+
     }
+
     else {
+
         printf("Input invalid!");
+
         menu();
+
     }
+
 }
+
+
 
 void calc(int choice){
+
     double num1;
+
     double num2;
+
     double sum;
+
     printf("Masukkan angka pertama : ");
+
     num1 = input();
+
     printf("Masukkan angka kedua : ");
+
     num2 = input();
+
     if(choice == 4 && num2 == 0){
+
         printf("Pembagi nol tidak diperbolehkan \n");
-    } else if(choice == 5){
+
+    }  
+	
+	else if(choice == 5){
+
         if(ceil(num1) != num1 || ceil(num2) != num2){
+
             printf("Untuk perhitnungan modulus input integer! \n");
+
         }
+        else {
+        	sum = modulus(num1,num2);
+        	printf("Hasil Perhitungan : %.4lf",sum);
+        	
+		}
+        
+
     } else {
+
         switch(choice){
+
                 case 1 :
+
                     sum = pertambahan(num1,num2);
+
                     break;
+
                 case 2 :
+
                     sum = pengurangan(num1,num2);
+
                     break;
+
                 case 3:
+
                     sum = perkalian(num1,num2);
+
                     break;
+
                 case 4:
+
                     sum = pembagian(num1,num2);
+
                     break;
-                case 5:
-                    sum = modulus(num1,num2);
-                    break;
+
             }
+
         printf("Hasil Perhitungan : %.4lf",sum);
+
     }
+
 }
+
+
 
 double pertambahan(double num1,double num2){
+	
     return num1 + num2;
+
 }
+
+
 
 double pengurangan(double num1,double num2){
+
     return num1-num2;
+
 }
+
+
 
 double perkalian(double num1,double num2){
+
     return num1*num2;
+
 }
+
+
 
 double pembagian(double num1,double num2){
+
     return num1/num2;
+
 }
+
+
 
 int modulus(double num1,double num2){
-    return (int)num1%(int)num2;
+	
+    return (int) num1 % (int) num2;
+
 }
+
+
 
 double input(){
+
     double num;
+
     char chara;
-    if (scanf("%lf%c",&num,&chara)!= 2 || chara != '\n' || chara == ','){
+
+    if (scanf("%lf%c",&num,&chara)!= 2  || chara != '\n' || chara == ','){
+
         clear_buffer();
+
         printf("Input invalid! Masukkan angka : ");
+
         return input();
+
     } else {
+
         return num;
+
     }
+
 }
 
+
+
 void clear_buffer(){
+
     char c;
+
     while ((c = getchar()) != '\n' && c != EOF) ; 
+
     
+
 }
+
+
 
 void reset(){
 
+
+
     int valid_reset;
+
     char parameter[1];
 
+
+
     printf("\n\nKembali menghitung?[Y/N] ");
+
     gets(parameter);
+
     
+
     if (!(strcmp(parameter,"Y")) || !(strcmp(parameter,"y"))){
+
         
+
         system("cls");
+
         menu();
 
+
+
     } else if (!(strcmp(parameter,"N")) || !(strcmp(parameter,"n"))){
+
         
+
         return;
 
+
+
     } else {
+
         system("cls");
+
         printf("Invalid Input !\n\a");
+
         reset();
+
     }
+
 }
