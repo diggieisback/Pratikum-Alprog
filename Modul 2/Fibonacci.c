5a9336fda62f2d7519ff6c947bf0b8e2bb33d7f5
aegishield <prasetyo.raharja12@gmail.com>
2018-03-25 13:01:16 +0800

    add itertive and recursive choice

--- Modul 2/Fibonacci.c
+++ Modul 2/Fibonacci.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 int fibonacci(int n);
-void array(int n);
+void arrayRec(int n);
+void arrayIter(int n);
+void menu();
 
 int main(){
-    int n;
-    scanf("%d",&n);
-    array(n);
+    menu();
     return 0;
+}
+
+void menu(){
+    int choice;
+    int n;
+    printf("Menu :\n1.Fibonacci iteratif\n2.Fibonacci rekursif\n3.Exit Program\n\nMasukkan pilihan : ");
+    scanf("%d",&choice);
+    printf("Masukkan panjang deret : ");
+    scanf("%d",&n);
+    switch(choice){
+        case 1:
+            arrayIter(n);
+            break;
+        case 2:
+            arrayRec(n);
+            break;
+    }
 }
 
 int fibonacci(int n){   
@@ -20,11 +37,26 @@ int fibonacci(int n){
     }
 }
 
-void array(int n){
+void arrayRec(int n){
     int array[n];
     printf("%d deret bilangan fibonacci : ",n);
     for (int i = 0;i < n; i++){
         array[i] = fibonacci(i);
         printf("%d ",array[i]);
     }
+}
+
+void arrayIter(int n){
+    int array[n];
+    for (int i = 0 ; i < n ; i++){
+        if(i == 0){
+            array[i] = 0;
+        } else if (i == 1){
+            array[i] = 1;
+        } else { 
+            array[i] = array[i-1] + array[i-2];
+        }
+        printf("%d ",array[i]);
+    }
+
 }
